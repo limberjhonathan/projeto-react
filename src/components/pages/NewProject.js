310d43f47a77cc1f1c6adbcf6e36006d02da9100
Limber Jhonathan <limberjhonathan@gmail.com>
2024-07-02 21:29:04 -0300

    Adição de new project + html

--- src/components/pages/NewProject.js
+++ src/components/pages/NewProject.js
@@ -1,3 +1,13 @@
+import ProjectForm from '../project/ProjectForm'
+import styles from './NewProjetc.module.css'
+
 export default function NewProject() {
-    return <h1>NewProject</h1>
+    return (
+        <div className={styles.newproject_container}>
+            <h1>Criar Projeto</h1>
+            <p>Crie seu projeto para depois adicionar os serviços</p>
+            <p>formulário</p>
+            <ProjectForm />
+        </div>
+    )
 }
