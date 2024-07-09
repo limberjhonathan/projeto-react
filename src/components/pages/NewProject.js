1a8e2a7f73993db45553d485a77fb87ed83826af
Limber Jhonathan <limberjhonathan@gmail.com>
2024-07-08 21:57:07 -0300

    project message

--- src/components/pages/NewProject.js
+++ src/components/pages/NewProject.js
@@ -23,7 +23,7 @@ export default function NewProject() {
             .then((resp) => resp.json())
             .then((data) => {
                 console.log(data)
-                navigate('/projects', { message: 'Projeto criado com sucesso!' });
+                navigate('/projects', { state: { message: 'Projeto criado com sucesso!' }})
             })
             .catch(err => console.log(err))
     }
--- src/components/pages/projects.js
+++ src/components/pages/projects.js
@@ -1,3 +1,19 @@
+import { useLocation } from "react-router-dom"
+
+import Message from "../layout/Message"
+
 export default function Projects(){
-    return <h1>Projects</h1>
+    const location = useLocation();
+    let message = ''
+    if(location.state){
+        message = location.state.message
+        console.log('message -------->', message)
+    }
+
+    return (
+        <div>
+            <h1>Meu Projetos</h1>
+            {message && <Message type="success" msg={message}/>}
+        </div>
+    )
 }
