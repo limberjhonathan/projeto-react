75ad66892f16ec05c3884b2914d9cfe5078ac68e
Limber Jhonathan <limberjhonathan@gmail.com>
2024-07-08 23:53:11 -0300

    buttom format

--- src/components/pages/projects.js
+++ src/components/pages/projects.js
@@ -1,6 +1,10 @@
 import { useLocation } from "react-router-dom"
 
 import Message from "../layout/Message"
+import Container from "../layout/Container";
+import LinkButton from "../layout/LinkButton";
+
+import styles from "./project.module.css"
 
 export default function Projects(){
     const location = useLocation();
@@ -11,9 +15,15 @@ export default function Projects(){
     }
 
     return (
-        <div>
-            <h1>Meu Projetos</h1>
+        <div className={styles.project_container}>
+            <div className={styles.title_container}>
+                <h1>Meu Projetos</h1>
+                <LinkButton to={'/newproject'} text="Criar Projeto"/>
+            </div>
             {message && <Message type="success" msg={message}/>}
+            <Container customClass='start'>
+                <p>Projetos...</p>
+            </Container>
         </div>
     )
 }
