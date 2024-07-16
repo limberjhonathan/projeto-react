0fd8d35d1c855a6785ae998230d42ff8cff0555b
Limber Jhonathan <limberjhonathan@gmail.com>
2024-07-15 21:59:40 -0300

    delete

--- src/components/pages/projects.js
+++ src/components/pages/projects.js
@@ -9,13 +9,14 @@ import ProjectCard from "../project/ProjectCards";
 import styles from "./project.module.css"
 import { useState, useEffect } from "react";
 
-export default function Projects(){
+export default function Projects() {
     const [projects, setProjects] = useState([])
     const [removeLoadingm, setRemoveLoading] = useState(false)
+    const [projectMessage, setProjectMessage] = useState('')
 
     const location = useLocation();
     let message = ''
-    if(location.state){
+    if (location.state) {
         message = location.state.message
         console.log('message -------->', message)
     }
@@ -28,31 +29,47 @@ export default function Projects(){
                     'Content-Type': 'aplication/json',
                 }
             })
-            .then((resp) => resp.json())
-            .then((data) => {
-                console.log(data)
-                setProjects(data)
-                setRemoveLoading(true)
-            })
-            .catch((err) => console.log(err))
+                .then((resp) => resp.json())
+                .then((data) => {
+                    console.log(data)
+                    setProjects(data)
+                    setRemoveLoading(true)
+                })
+                .catch((err) => console.log(err))
         }, 300)
     }, [])
+
+    function removeProject(id) {
+        fetch(`http://localhost:5000/projects/${id}`, {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'aplication/json'
+            },
+        }).then(resp => resp.json())
+        .then( () => {
+            setProjects(projects.filter((projects) => projects.id !== id))
+            setProjectMessage('Projeto removido  com sucesso!')
+        })
+        .catch(err => console.log(err))
+    }
 
     return (
         <div className={styles.project_container}>
             <div className={styles.title_container}>
                 <h1>Meu Projetos</h1>
-                <LinkButton to={'/newproject'} text="Criar Projeto"/>
+                <LinkButton to={'/newproject'} text="Criar Projeto" />
             </div>
-            {message && <Message type="success" msg={message}/>}
+            {message && <Message type="success" msg={message} />}
+            {projectMessage && <Message type="success" msg={projectMessage} />}
             <Container customClass='start'>
                 {projects.length > 0 && projects.map((project) => (
                     <ProjectCard
-                        id={project.id} 
+                        id={project.id}
                         name={project.name}
                         budget={project.budget}
                         category={project.category.name}
                         key={project.id}
+                        handleRemove={removeProject}
                     />
                 ))}
                 {!removeLoadingm && <Loading />}{
